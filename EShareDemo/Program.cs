4265bed2ed7e3fc0919eea462d0136be05a3cf39
DavidBeh <67109172+DavidBeh@users.noreply.github.com>
2023-01-26 12:18:14 +0000

    demo

---
+++ EShareDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Runtime.InteropServices;
+using EShareDemo;
+using System.Net;
+using System.IO;
+using System.Reflection;
+
+Console.WriteLine("IP Eingeben");
+
+var ipstr = Console.ReadLine();
+
+
+IPAddress ipaddr = IPAddress.Parse(ipstr);
+EShareClient c = new EShareClient(ipaddr);
+var cap = await c.GetScreenCapture();
+File.WriteAllBytes("screencapture.png", cap.ToArray());
+System.Diagnostics.Process.Start("explorer.exe", "screencapture.png");
